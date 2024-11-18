4d701bec2161e447479ea8a9da840f9791103317
Lucas Fernandes Lima <lucas.liima030@gmail.com>
2024-11-18 16:09:24 -0300

    feat(backend/todo): add error middleware

--- apps/backend/src/shared/domain/repositories/repository.interface.ts
+++ apps/backend/src/shared/domain/repositories/repository.interface.ts
@@ -3,7 +3,5 @@ import { BaseEntity } from '../entities';
 export interface Repository<E extends BaseEntity> {
   create(entity: E): Promise<void>;
   update(entity: E): Promise<void>;
-  delete(entity: E): Promise<void>;
   getById(id: string): Promise<E | null>;
-  getAll(): Promise<E[]>;
 }
