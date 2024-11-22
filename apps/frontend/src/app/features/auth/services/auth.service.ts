a85e4b208b36c7bc23169cb5d9f9eeffddf6cd89
Lucas Fernandes Lima <lucas.liima030@gmail.com>
2024-11-22 19:21:47 -0300

    feat(frontend/auth): save users data in storage

--- apps/frontend/src/app/features/auth/services/auth.service.ts
+++ apps/frontend/src/app/features/auth/services/auth.service.ts
@@ -3,6 +3,16 @@ import { Injectable } from '@angular/core';
 import { Observable, tap } from 'rxjs';
 import { STORAGE_KEYS } from '../../../core/utils/storage-keys';
 
+export type User = {
+  userId: string;
+  name: string;
+  email: string;
+}
+
+type LoginResponse = User & {
+  token: string;
+};
+
 @Injectable({
   providedIn: 'root'
 })
@@ -12,10 +22,17 @@ export class AuthService {
     private readonly http: HttpClient,
   ) { }
 
-  public login(email: string, password: string): Observable<{ token: string }> {
-    return this.http.post<{ token: string }>('/users/login', { email, password })
+  public login(email: string, password: string): Observable<LoginResponse> {
+    return this.http.post<LoginResponse>('/users/login', { email, password })
       .pipe(
         tap(({ token }) => localStorage.setItem(STORAGE_KEYS.TOKEN, token)),
+        tap(response => {
+          localStorage.setItem(STORAGE_KEYS.USER, JSON.stringify({
+            userId: response.userId,
+            name: response.name,
+            email: response.email,
+          }));
+        }),
       );
   }
 
