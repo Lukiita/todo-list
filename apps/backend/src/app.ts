44d541607ad4a6a2a0104c8904d5be50f3ea5a60
Lucas Fernandes Lima <lucas.liima030@gmail.com>
2024-11-22 00:18:45 -0300

    chore(backend): convert google credentials para json

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -20,7 +20,7 @@ import {
   router as userRoutes
 } from './user';
 
-const serviceAccount = process.env.GOOGLE_APPLICATION_CREDENTIALS as string;
+const serviceAccount = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS || '{}');
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
